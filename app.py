f8f488f6bc65d979959c859d66e377d504b28b60
Znobishchev <aleksiy909@mail.ru>
2025-11-19 10:53:46 +0300

    Обновил текст на главной странице

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Лабораторная работа выполнена!</h1><p>Приложение работает на Render (аналог Heroku).</p>"
+   return "<h1>Версия 2.0</h1><p>Обновил код, сделал git push</p>"
 
 if __name__ == '__main__':
-    # Получаем порт, который выдаст облако, или используем 5000 по умолчанию
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
